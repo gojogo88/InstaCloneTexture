5114d0fe475b710d28e2d990a23277bdf9e201aa
Jonathan Go <gojonathan.dev@gmail.com>
2020-04-30 13:20:21 +0800

    Initial Commit

---
+++ InstaCloneTexture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaCloneTexture
+//
+//  Created by Jonathan Go on 4/30/20.
+//  Copyright © 2020 SonnerStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
